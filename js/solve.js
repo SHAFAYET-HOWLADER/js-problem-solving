1cacf59b68f9b29090077c6073411b4cce450704
unknown <you@example.com>
2022-03-07 22:31:39 +0600

    commited

--- js/solve.js
+++ js/solve.js
@@ -18,3 +18,36 @@ if(years >= 45 && savingMoney >= 50000 && hasFlatt === true){
 else{
   console.log("please fill above all requirement to marry");
 }
+
+//problem : 3 <------- || operator------->
+if(years >= 45 || savingMoney >= 60000 || hasFlatt === true){
+    console.log("you are so adult, please marry her");
+ }
+ else{
+   console.log("please fill above all requirement to marry");
+ }
+
+//problem : 4 <------- multiple condition ------->
+const characterOfLover1 =100;
+const characterOfLover2 = 80;
+const characterOfLover3 = 42;
+if(characterOfLover1 > characterOfLover2 && characterOfLover1 > characterOfLover3){
+   console.log("akhi love me enough");
+}
+else if(characterOfLover2 > characterOfLover1 || characterOfLover2 > characterOfLover3){
+   console.log("munni does not love me pure enough");
+}
+else{
+    console.log("behind the seen sabiha loved me more than my expectation");
+}
+
+//problem : 5 <------- basic array  ------->
+const numbers  = [5, 2, 6, 3,4, 9, 0, 7, 10, 12, 15];
+numbers.unshift(4);
+console.log(numbers);
+const length = numbers.length;
+const checkIndex = numbers[10];
+const checkIndexOf = numbers.indexOf(15);
+console.log(checkIndexOf);
+console.log(checkIndex);
+console.log(length)
