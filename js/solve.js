1c439c37abfe78091985ae91800e1b18d5e244f0
unknown <you@example.com>
2022-03-07 22:06:56 +0600

    second problem added

--- js/solve.js
+++ js/solve.js
@@ -1,7 +1,20 @@
 
 //problem : 1 <-------ternary operator------->
 const age = 18;
+const loggedIn = 5;
 const check = (age >= 20) ? "you are adult" : age ? "true" : "false";
-console.log(check)
+const access = loggedIn >= 5;
+console.log(access);
+console.log(check);
 
-//problem : 2 <-------ternary operator------->
+//problem : 2 <-------&& operator------->
+const years = 25;
+const savingMoney = 50000;
+const hasFlatt = true;
+
+if(years >= 45 && savingMoney >= 50000 && hasFlatt === true){
+   console.log("you are so adult, please marry her");
+}
+else{
+  console.log("please fill above all requirement to marry");
+}
