d835b47eda034992d42b55abc3aa35882d120d03
unknown <you@example.com>
2022-03-07 21:54:07 +0600

    problem-1 added

---
+++ js/solve.js
@@ -0,0 +1,7 @@
+
+//problem : 1 <-------ternary operator------->
+const age = 18;
+const check = (age >= 20) ? "you are adult" : age ? "true" : "false";
+console.log(check)
+
+//problem : 2 <-------ternary operator------->
